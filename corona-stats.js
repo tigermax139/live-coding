f273362d8cfacba8bb5754b3e4702601104294ec
Bohdan Yurchuk <tigermax139@gmail.com>
2024-08-06 17:31:47 +0300

    Initial commit

---
+++ corona-stats.js
@@ -0,0 +1,21 @@
+// TODO 1. load corona stats data
+// TODO 2. iterate over data and count tests result grouped by locationId
+// TODO 3 (Optional). find locations with min and max positive tests
+
+
+/**
+ Expected output:
+ const result = {
+ 1: {
+ positive: 4,
+ negative: 7,
+ pending: 8,
+ },
+ //  ...
+ 9: {
+ positive: 2,
+ negative: 0,
+ pending: 44,
+ },
+ }
+ * */
---
+++ image-url.js
@@ -0,0 +1,34 @@
+// TODO implement wait function
+/**
+ *
+ await wait(100); // execution wait 100ms
+ nextCode(); // stated after 100ms
+ * */
+const wait = () => {};
+
+const DEFAULT_IMAGE_URL = 'https://example-default.png';
+const GET_IMAGE_TIMEOUT = 300;
+
+const getImageUrl = async (id) => {
+    const random = Math.random() * 1000;
+    await wait(random);
+    return `https://example-${id}.png`;
+}
+
+const imageIds = [1, 2, 3, 4, 5];
+
+const processImages = () => {
+    // TODO resolve image url for every item of imageIds array
+    // TODO process images parallel
+    // TODO if resolving process executes more that GET_IMAGE_TIMEOUT - return DEFAULT_IMAGE_URL instead
+};
+/**
+ Example output:
+ [
+ 'https://example-1.png',
+ 'https://example-3.png',
+ 'https://example-default.png',
+ 'https://example-default.png',
+ 'https://example-5.png',
+ ]
+ * */
